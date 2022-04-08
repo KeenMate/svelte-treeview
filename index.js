d3bd6db483272d2f25329eea2da159706c957c8a
Jan Rada <31016933+ZelvaMan@users.noreply.github.com>
2022-04-08 23:49:11 +0200

    added export for MenuOption and MenuDivider

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 
 export {default as TreeView} from "./src/Treeview.svelte"
 export {searchTree,mergeTrees} from "./src/TreeHelpers"
+export {default as MenuOption} from "./src/MenuOption.svelte"
+export  {default as MenuDivider} from "./src/MenuDivider.svelte"
