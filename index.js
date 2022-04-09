53ea2246085e9a7e20d311d10b6a654da6de84e3
Jan Rada <31016933+ZelvaMan@users.noreply.github.com>
2022-04-09 13:15:22 +0200

    fixed typo in index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-export {default as TreeView} from "./src/Treeview.svelte"
+export {default as TreeView} from "./src/TreeView.svelte"
 export {searchTree,mergeTrees} from "./src/TreeHelpers"
 export {default as MenuOption} from "./src/MenuOption.svelte"
 export  {default as MenuDivider} from "./src/MenuDivider.svelte"
