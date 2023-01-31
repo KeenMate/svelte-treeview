5a9080ff8efc2c1e2284bac938d8a1d60d7e06f0
Jan Rada <31016933+ZelvaMan@users.noreply.github.com>
2023-01-31 20:33:45 +0100

    merge name props into propNames

---
+++ src/consts.js
@@ -0,0 +1,12 @@
+export const defaultPropNames = {
+	nodePathProperty: "nodePath",
+	hasChildrenProperty: "hasChildren",
+	expandedProperty: "__expanded",
+	selectedProperty: "__selected",
+	useCallbackProperty: "__useCallback",
+	priorityProperty: "priority",
+	isDraggableProperty: "isDraggable",
+	insertDisabledProperty: "insertDisabled",
+	nestDisabledProperty: "nestDisabled",
+	checkboxVisibleProperty: "checkboxVisible",
+};
