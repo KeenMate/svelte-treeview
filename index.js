6be721c71324d5f5681c76540d694698bb116152
Jan Rada <31016933+ZelvaMan@users.noreply.github.com>
2023-01-31 21:38:50 +0100

    renamed tree helpers

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 export { default as TreeView } from "./src/TreeView.svelte";
-export { TreeHelper } from "./src/TreeHelpers";
+export { TreeHelper } from "./src/helpers/tree-helper";
 export { default as MenuOption } from "./src/MenuOption.svelte";
 export { default as MenuDivider } from "./src/MenuDivider.svelte";
