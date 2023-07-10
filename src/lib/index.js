5e18f4d8d465c5353134d4f2dae8899052bef3b1
Jan Rada <31016933+ZelvaMan@users.noreply.github.com>
2023-07-10 22:20:03 +0200

    update deps, fix export

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,3 +2,4 @@ export { default as TreeView } from './TreeView.svelte';
 export { default as MenuDivider } from './menu/MenuDivider.svelte';
 export { default as MenuOption } from './menu/MenuOption.svelte';
 export { TreeHelper } from './helpers/tree-helper.js';
+export * as Constants from './consts.js';
